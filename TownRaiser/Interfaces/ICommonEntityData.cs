5141adf9eb6e65f6fc83bd45b595503f99128b57
Adam Patridge <patridge@gmail.com>
2019-04-27 22:31:02 -0600

    Initial Desktop GL version

---
+++ TownRaiser/Interfaces/ICommonEntityData.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TownRaiser.Entities;
+using static TownRaiser.GumRuntimes.IconButtonRuntime;
+
+namespace TownRaiser.Interfaces
+{
+    public interface ICommonEntityData
+    {
+        Microsoft.Xna.Framework.Input.Keys Hotkey { get; }
+        string DataName { get; }
+        string MenuTitleDisplay { get; }
+        int Gold { get; }
+        int Lumber { get; }
+        int Stone { get; }
+        int CapacityUsed { get; }
+        bool ShouldEnableButton(int lumber, int stone, int gold, int currentCapacity, int maxCapacity, IEnumerable<Building> existingBuildings, IUpdatesStatus entityCreatedFrom);
+    }
+}
